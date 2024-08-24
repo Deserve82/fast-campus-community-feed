eb2198cf1f83fbe5f50858802ae921e93bf2b274
deserve82 <inth8204@gmail.com>
2024-08-24 20:01:31 +0900

    fix : 인수 테스트 진행되도록 변경

--- src/main/java/org/fastcampus/community_feed/common/idempotency/repository/entity/IdempotencyEntity.java
+++ src/main/java/org/fastcampus/community_feed/common/idempotency/repository/entity/IdempotencyEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import org.fastcampus.community_feed.common.idempotency.Idempotency;
 import org.fastcampus.community_feed.common.utils.ResponseObjectMapper;
 
 @Entity
+@Table(name="community_idempotency")
 @NoArgsConstructor
 @AllArgsConstructor
 public class IdempotencyEntity {
